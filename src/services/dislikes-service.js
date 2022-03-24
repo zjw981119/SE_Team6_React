19b295647b2c8da561039b3493076eb094cf24d8
zjw981119 <962610351@qq.com>
2022-03-24 15:09:06 -0400

    Modify documentation

--- src/services/dislikes-service.js
+++ src/services/dislikes-service.js
@@ -1,3 +1,6 @@
+/**
+ * @file Axios Request service API for dislikes && tuits resource
+ */
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -7,6 +10,11 @@ const api = axios.create({
     withCredentials: true
 });
 
+/**
+ * Update tuit stats based on user's click event(toggles dislike button)
+ * @param {string} uid Represents user that is toggling dislike button
+ * @param {string} tid Represents the tuit being disliked by user
+ */
 export const userDislikesTuit = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data);
