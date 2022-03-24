aa6d6ab156076eb6a11559236ec8876c5c1ca89c
zjw981119 <962610351@qq.com>
2022-03-24 15:01:35 -0400

    inplement like/dislike highlight && unhighlight according to whether the login user like/dislike the specific tuit or not

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -10,8 +10,3 @@ const api = axios.create({
 export const userLikesTuit = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
         .then(response => response.data);
-
-// check if user likes tuit
-export const findUserLikesTuit = (uid, tid) =>
-    api.get(`${USERS_API}/${uid}/dislikes/${tid}`)
-        .then(response => response.data);
