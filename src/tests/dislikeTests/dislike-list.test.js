9edcc8ae45c055e904badd8486efbe8a24551a11
zjw981119 <962610351@qq.com>
2022-03-25 17:13:47 -0400

    Implement tests for findTuitsDislikedByUser

---
+++ src/tests/dislikeTests/dislike-list.test.js
@@ -0,0 +1,98 @@
+// sample tuits
+import {render, screen} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+import Tuits from "../../components/tuits";
+import axios from "axios";
+import {findAllTuits} from "../../services/tuits-service";
+import {findAllTuitsDisLikedByUser} from "../../services/dislikes-service";
+import {login} from "../../services/security-service";
+
+const MOCKED_TUITS_I_DISLIKED = [
+    {
+        _id: "123",
+        tuit: "bob's first tuit",
+        postedBy: {
+            _id: "bob2222",
+            username: 'bob',
+            password: 'bob123',
+            email: 'bob@weyland.com',
+        },
+        stats:{
+            replies: 0,
+            retuits: 0,
+            likes: 0,
+            dislikes: 2
+        },
+        postedOn: "2022-03-20T20:24:10.481Z"
+    },
+    {
+        _id: "234",
+        tuit: "bob's second tuit",
+        postedBy: {
+            _id: "bob2222",
+            username: 'bob',
+            password: 'bob123',
+            email: 'bob@weyland.com',
+        },
+        stats:{
+            replies: 0,
+            retuits: 0,
+            likes: 0,
+            dislikes: 3
+        },
+        postedOn: "2022-03-21T20:24:10.481Z"
+    },
+];
+
+const LOGIN_USER = {
+    username: "alice",
+    password: "alice123"
+}
+//test static tuit array
+test('dislike list renders static tuit array', () => {
+    // render a tuit array
+    render(
+        <HashRouter>
+            <Tuits tuits={MOCKED_TUITS_I_DISLIKED}/>
+        </HashRouter>);
+    // verify dislike number appears in screen somewhere
+    const linkElement = screen.getByText(/2/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+// test rendering from REST
+test('dislike list renders async', async () => {
+    //user login
+    await login(LOGIN_USER)
+    const tuits = await findAllTuitsDisLikedByUser("me");
+    // render a dislike tuit array
+    render(
+        <HashRouter>
+            <Tuits tuits={tuits}/>
+        </HashRouter>);
+    // verify dislike number appears in screen somewhere
+    const linkElement = screen.getByText(/1/i);
+    expect(linkElement).toBeInTheDocument();
+})
+
+test('dislike list renders mocked', async () => {
+    //only mock axios.get() method
+    const mock = jest.spyOn(axios, 'get');
+    // simulate response from REST with static response
+    mock.mockImplementation(() =>
+        Promise.resolve({data: {tuits: MOCKED_TUITS_I_DISLIKED}}));
+    const response = await findAllTuits();
+    const tuits = response.tuits;
+
+    // render a dislike tuit array
+    render(
+        <HashRouter>
+            <Tuits tuits={tuits}/>
+        </HashRouter>);
+    // verify dislike number appears in screen somewhere
+    const linkElement = screen.getByText(/3/i);
+    expect(linkElement).toBeInTheDocument();
+
+    // restore original implementation
+    // mock.mockRestore();
+});
--- src/tests/dislikeTests/dislikes-service.test.js
+++ src/tests/dislikeTests/dislikes-service.test.js
@@ -4,7 +4,7 @@
 import {deleteUsersByUsername} from "../../services/users-service";
 import {createTuit, deleteTuitByContent, findTuitById} from "../../services/tuits-service";
 import {logout, signup} from "../../services/security-service";
-import {userDislikesTuit} from "../../services/dislikes-service";
+import {findAllTuitsDisLikedByUser, userDislikesTuit} from "../../services/dislikes-service";
 import {userLikesTuit} from "../../services/likes-service";
 
 // sample user to insert
@@ -19,6 +19,9 @@ const tuit = {
     tuit: 'test tuit',
 };
 
+const tuit2 = {
+    tuit: 'test tuit2'
+}
 describe('toggleDislike', () => {
 
     // setup test before running test
@@ -109,6 +112,9 @@ describe('sameUserToggleLikeBeforeDislike', () => {
         updatedTuit = await findTuitById(newTuit._id);
         expect(updatedTuit.stats.likes).toEqual(0);
         expect(updatedTuit.stats.dislikes).toEqual(1);
+
+        //toggle dislike, delete dislike test data in database
+        await userDislikesTuit(loginUser._id, newTuit._id);
     });
 
 });
@@ -156,7 +162,60 @@ describe('sameUsertoggleDislikeBeforeLike', () => {
         updatedTuit = await findTuitById(newTuit._id);
         expect(updatedTuit.stats.likes).toEqual(1);
         expect(updatedTuit.stats.dislikes).toEqual(0);
+
+        //toggle like, delete like test data in database
+        await userLikesTuit(loginUser._id, newTuit._id);
     });
 
 });
 
+//test getTuitsDislikedByUser
+describe('getTuitsDislikedByUser', () => {
+
+    // setup test before running test
+    beforeAll(() => {
+        const promise1 = deleteUsersByUsername(user.username);
+        const promise2 = deleteTuitByContent(tuit.tuit)
+        const promise3 = deleteTuitByContent(tuit2.tuit)
+        // pass an array to Promise.all()
+        return Promise.all([promise1, promise2, promise3]);
+    })
+
+    // clean up after test runs
+    afterAll(() => {
+        // remove any data we created
+        const promise1 = deleteUsersByUsername(user.username);
+        const promise2 = deleteTuitByContent(tuit.tuit)
+        const promise3 = deleteTuitByContent(tuit2.tuit)
+        const promise4 = logout()
+        return Promise.all([promise1, promise2, promise3, promise4]);
+    })
+
+
+    test('can find tuits disliked by user', async () => {
+        // extend timeout
+        jest.setTimeout(10000)
+        //user signup, create session
+        const loginUser = await signup(user);
+        //login user create tuit
+        const newTuit1 = await createTuit(loginUser._id, tuit)
+        const newTuit2 = await createTuit(loginUser._id, tuit2)
+
+        //login user toggle dislikes two tuits
+        await userDislikesTuit(loginUser._id, newTuit1._id);
+        await userDislikesTuit(loginUser._id, newTuit2._id);
+
+        //find tuits disliked by login user
+        const dislikedTuits = await findAllTuitsDisLikedByUser(loginUser._id)
+        //verify tuits number
+        expect(dislikedTuits.length).toEqual(2);
+        //verify tuits content
+        expect(dislikedTuits[0].tuit).toEqual(newTuit1.tuit);
+        expect(dislikedTuits[1].tuit).toEqual(newTuit2.tuit);
+
+        //undislike tuits, clear data in dislikes database table
+        await userDislikesTuit(loginUser._id, newTuit1._id);
+        await userDislikesTuit(loginUser._id, newTuit2._id);
+    });
+
+});
